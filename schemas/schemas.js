785d799525e80d0f68cc759998ea5fd0d3f6a912
Inna-Melnyk <inamelnyk@ukr.net>
2023-08-30 23:32:09 +0200

    name

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,9 +1,7 @@
 const Joi = require("joi");
 
- const addSchema = Joi.object({
+export const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.string().required(),
- });
-
-module.exports = addSchema;
+});
